5ee5f74bc8ed9e47103053e9b16b037e8ab1780a
yhw2003 <321612813@qq.com>
2022-11-16 09:35:59 +0800

    start

---
+++ include/fold.h
@@ -0,0 +1,5 @@
+#define dutyCycle(a) ((a) * 65536) / 100
+#define LF 0
+#define RF 1
+#define LB 2
+#define RB 3
--- src/main.cpp
+++ src/main.cpp
@@ -1,79 +1,28 @@
 #include <Arduino.h>
 #include "AutoPID.h"
+#include "ATtiny_PWM.h"
+#include "fold.h"
+#define UNKOWN_PIN
 
-class motor
-{
-private:
-    /* data */
-  uint32_t pinfst;
-  uint32_t pinsec;
+const int targetSpeed = 100;
+int realDutyCycle = 0.5;
+int roundCnt = 0;
 
-public:
-  motor(uint32_t pin1, uint32_t pin2)
-  {
-      pinfst = pin1;
-      pinsec = pin2;
-      pinMode(pinfst,OUTPUT);
-      pinMode(pinsec,OUTPUT);
-      _stop();
-  }
-  void _stop(){
-    digitalWrite(pinfst,LOW);
-    digitalWrite(pinsec,LOW);
-  }
-  void goCW()
-  {
-    _stop();
-    digitalWrite(pinfst,HIGH);
-  }
-  void goAntiCW()
-  {
-    _stop();
-    digitalWrite(pinsec,HIGH);
-  }
-};
+ATtiny_PWM* speed_controllers[4];
 
-motor LFm(PA0,PA1);
-motor RFm(PA2,PA3);
-motor LBm(PA4,PA5);
-motor RBm(PA6,PA7);
 
-void go(motor * Tcar)
-{
+void setup() {
+  attachInterrupt(UNKOWN_PIN,counter,FALLING);
   for (size_t i = 0; i < 4; i++)
   {
-    Tcar[i].goCW();
-  }
-}
-
-void stop(motor * Tcar)
-{
-  for (size_t i = 0; i < 4; i++)
-  {
-    Tcar[i]._stop();
+    speed_controllers[i] = new ATtiny_PWM(A0+i,1000.0f,dutyCycle(realDutyCycle));
   }
   
 }
 
-void setup() {
-  // put your setup code here, to run once:
-  pinMode(PB0,INPUT_PULLDOWN);
-
+void loop() {
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-  motor Tcar[] = {LFm,RFm,LBm,RBm};
-  delay(100);
-  while (1)
-  {
-    if (digitalRead(PB0))
-    {
-      stop(Tcar);
-    } else
-    {
-      // break;
-      go(Tcar);
-    }
-  }
-}  
+void counter() {
+  roundCnt++;
+}
